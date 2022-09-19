af6b916accbfaa61a60c0e3668f66df2b4abbc4f
Sierra Harley <sharley777@yahoo.com>
2022-09-18 21:34:26 -0400

    uptolab2completed

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+console.log('Hey world Eye Am Nù');
+
+let visitorname = prompt('What is your name?');
+
+alert(`You are now entering a Nù dimension ${visitorname}! Guess yes or no to the following questions`);
+
+let questionOneGuess = prompt('Do I have a pet?' + 'Yes or no').toLowerCase();
+
+if (questionOneGuess === 'y' || questionOneGuess === ' yes '){
+  alert('You are correct.');
+} else if (questionOneGuess === 'n' || questionOneGuess === 'no') {
+  alert('Sorry, answer incorrect');
+}
+
+let questionTwoGuess = prompt('Did I graduate college?' + 'Yes or no').toLowerCase();
+
+if (questionTwoGuess === 'y' || questionTwoGuess === 'yes'){
+  alert('Ding Ding Ding!');
+} else if (questionTwoGuess === 'n' || questionTwoGuess === 'no'){
+  alert('Nope, not quite');
+}
+
+let questionThreeGuess = prompt('Are you present?' + 'Yes or No.').toLowerCase();
+
+if (questionThreeGuess === 'y' || questionThreeGuess === ' yes '){
+  alert('You are correct.');
+} else if (questionThreeGuess === 'n' || questionThreeGuess === 'no') {
+  alert('Ahh ah ah. Try again.');
+}
+
+let questionFourGuess = prompt('Green is my favorite color.' + 'Yes or no.').toLowerCase();
+
+if (questionFourGuess === 'y' || questionFourGuess === ' yes '){
+  alert('You are correct.');
+} else if (questionFourGuess === 'n' || questionFourGuess === 'no') {
+  alert('Almost, not quite');
+}
+
+let questionFiveGuess = prompt('Green is my favorite color.' + 'Yes or no.').toLowerCase();
+
+if (questionFiveGuess === 'y' || questionFiveGuess === ' yes '){
+  alert('You are correct.');
+} else if (questionFiveGuess === 'n' || questionFiveGuess === 'no') {
+  alert('Last guess.');
+}
+
+
+alert(`Say cheese. Participation much obliged ${visitorname}.`);
+
+// Me llama Nù Harley and I only took a couple of classes of Spanish in high school. I am mature for my age; as I skipped the 5th grade because I would get dont with my work early and help (bother according to the teacher) other students. What's funny is I got in trouble in my junior/senior year of college for overly participating; how funny is that. I graduated from The Central State University, a historically black university, with a Bachelor's Degree of Science in Psychology and a minor in Business Administration. Always enjoyed school and a garden tool for knowledge. I love the brain and all of its functions.  Out of college I started out as a substitute teacher at a local city high school by my school and eventually assisted younger grade levels. I fell for the first graders as I had pessismistic thoughts. Firt corporate job was with Huntington National Bank as a teller but quit months in due to a purposely micromanaging manager. Told myself no more corporate jobs due to the restriction and rules I have to abide by that I already know to follow as a person of morals. A goal I have is to become the most thorough, efficient web developer/coder I work myself to be. Networking is my second highest priority aside from learning code. Lastly, creating a legacy for generations after me. All things Nù.//
